a796d1748175523617add59190d884444e4eda85
Ray Messina <rmessina10@gmail.com>
2021-01-20 00:14:02 -0600

    Picking an Image pt2 (register form)

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -48,16 +48,14 @@ class LoginTab extends Component {
 
     componentDidMount() {
         SecureStore.getItemAsync('userinfo')
-            .then(
-                userdata => {
-                    const userinfo = JSON.parse(userdata);
-                    if (userinfo) {
-                        this.setState({ username: userinfo.username });
-                        this.setState({ password: userinfo.password });
-                        this.setState({ remember: true });
-                    }
-
-                });
+            .then(userdata => {
+                const userinfo = JSON.parse(userdata);
+                if (userinfo) {
+                    this.setState({ username: userinfo.username });
+                    this.setState({ password: userinfo.password });
+                    this.setState({ remember: true })
+                }
+            });
     }
 
     render() {
@@ -125,6 +123,24 @@ class LoginTab extends Component {
 }
 
 class RegisterTab extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            firstName: '',
+            lastName: '',
+            email: '',
+            remember: false,
+            imageURL: baseUrl + 'images/logo.png'
+
+
+
+
+        }
+    }
+
     static navigationOptions = {
         title: 'Register',
         tabBarIcon: ({ tintColor }) => (
@@ -136,10 +152,91 @@ class RegisterTab extends Component {
         )
     }
 
+    handleRegister() {
+        if (this.state.remember) {
+            console.log(JSON.stringify(this.state));
+            SecureStore.setItemAsync(
+                'userinfo',
+                JSON.stringify({
+                    username: this.state.username,
+                    password: this.state.password
+                })
+            ).catch(error => console.log('Could not save user info', error));
+        } else {
+            SecureStore.deleteItemAsync('userinfo').catch(error => console.log('Could not delete user info', error));
+        }
+    }
+
     render() {
 
         return (
-            <ScrollView />
+            <ScrollView>
+                <View style={styles.container} >
+                    <Input
+                        placeholder="Username"
+                        leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+                        onChangeText={username => this.setState({ username: username })}
+                        value={this.state.username}
+                        containerStyle={styles.formInput}
+                        leftIconContainerStyle={styles.formIcon}
+                    />
+                    <Input
+                        placeholder="Password"
+                        leftIcon={{ type: 'font-awesome', name: 'key' }}
+                        onChangeText={password => this.setState({ password: password })}
+                        value={this.state.password}
+                        containerStyle={styles.formInput}
+                        leftIconContainerStyle={styles.formIcon}
+                    />
+                    <Input
+                        placeholder="First Name"
+                        leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+                        onChangeText={firstName => this.setState({ firstName: firstName })}
+                        value={this.state.firstName}
+                        containerStyle={styles.formInput}
+                        leftIconContainerStyle={styles.formIcon}
+                    />
+                    <Input
+                        placeholder="Last Name"
+                        leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+                        onChangeText={lastName => this.setState({ lastName: lastName })}
+                        value={this.state.lastName}
+                        containerStyle={styles.formInput}
+                        leftIconContainerStyle={styles.formIcon}
+                    />
+                    <Input
+                        placeholder="Email"
+                        leftIcon={{ type: 'font-awesome', name: 'envelope-o' }}
+                        onChangeText={email => this.setState({ email: email })}
+                        value={this.state.email}
+                        containerStyle={styles.formInput}
+                        leftIconContainerStyle={styles.formIcon}
+                    />
+                    <CheckBox
+                        title="Remember me"
+                        center
+                        checked={this.state.remember}
+                        onPress={() => this.setState({ remember: !this.state.remember })}
+                        containerStyle={styles.formCheckbox}
+                    />
+                    <View style={styles.formButton}>
+                        <Button
+                            onPress={() => this.handleRegister()}
+                            title='Register'
+                            icon={
+                                <Icon
+                                    name='user-plus'
+                                    type='font-awesome'
+                                    color='#fff'
+                                    iconStyle={{ marginRight: 10 }}
+                                />
+                            }
+                            buttonStyle={{ backgroundColor: '#5637DD' }}
+
+                        />
+                    </View>
+                </View>
+            </ScrollView>
         )
     }
 
@@ -163,20 +260,33 @@ const Login = createBottomTabNavigator({
 const styles = StyleSheet.create({
     container: {
         justifyContent: 'center',
-        margin: 20
+        margin: 10
     },
     formIcon: {
         marginRight: 10
     },
     formInput: {
-        padding: 10
+        padding: 8
     },
     formCheckbox: {
-        margin: 10,
+        margin: 8,
         backgroundColor: null
     },
     formButton: {
-        margin: 40
+        margin: 20,
+        marginRight: 40,
+        marginLeft: 40
+    },
+    imageContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        margin: 10
+    },
+    image: {
+        width: 60,
+        height: 60
     }
 });
 
